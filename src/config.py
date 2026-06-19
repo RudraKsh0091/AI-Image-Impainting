e7323f520bea9321f7067c565321324ed83cbe7d
RudraKsh0091 <rudraa.ln00@gmail.com>
2026-06-19 20:21:24 +0530

    Updated requirements.txt

--- src/config.py
+++ src/config.py
@@ -1,15 +1,7 @@
-# IMAGE_DIR = "/kaggle/input/datasets/awsaf49/coco-2017-dataset/coco2017/train2017"
-# IMG_SIZE = 256
-# BATCH_SIZE = 16
-# EPOCHS = 30
-# NUM_TRAIN = 36000
-# NUM_VAL = 4000
-# LEARNING_RATE = 2e-4
-
 IMAGE_DIR = "/kaggle/input/datasets/awsaf49/coco-2017-dataset/coco2017/train2017"
 IMG_SIZE = 256
 BATCH_SIZE = 16
-EPOCHS = 2
-NUM_TRAIN = 200
-NUM_VAL = 50
+EPOCHS = 30
+NUM_TRAIN = 36000
+NUM_VAL = 4000
 LEARNING_RATE = 2e-4
