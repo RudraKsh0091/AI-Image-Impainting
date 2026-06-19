ab1796d4c4e4db600fa26a6d4dbaba0207f2cb21
RudraKsh0091 <rudraa.ln00@gmail.com>
2026-06-19 19:48:54 +0530

    Updated data.py

--- src/data.py
+++ src/data.py
@@ -5,6 +5,14 @@ import os
 import numpy as np
 from src.masks import generate_mask
 
+def get_train_val_split(image_dir, num_train=36000, num_val=4000, seed=42):
+    all_paths = glob.glob(os.path.join(image_dir, "*.jpg"))
+    random.seed(seed)
+    random.shuffle(all_paths)
+    val_paths = all_paths[:num_val]
+    train_paths = all_paths[num_val:num_val + num_train]
+    return train_paths, val_paths
+
 def add_masks(image, img_size=256, mask_type='combined'):
     def _generate(img):
         mask = generate_mask(img_size=img_size, mask_type=mask_type)
@@ -18,31 +26,29 @@ def add_masks(image, img_size=256, mask_type='combined'):
     
     return (masked_image, mask), image
 
-def load_dataset(image_dir, num_images=40000, img_size=256, batch_size=16, mask_type='combined', seed=42):
-    # collect all jpg paths
-    all_paths = glob.glob(os.path.join(image_dir, "*.jpg"))
+def parse_image(path, img_size=256):
+    image = tf.io.read_file(path)
+    image = tf.image.decode_jpeg(image, channels=3)
+    image = tf.image.resize(image, [img_size, img_size])
+    image = tf.cast(image, tf.float32)
+    image = (image / 127.5) - 1.0
+    return image
+
+def load_dataset(paths, img_size=256, batch_size=16, mask_type='combined', fixed_seed=None, shuffle=True):
+    dataset = tf.data.Dataset.from_tensor_slices(paths)
     
-    # random sample
-    random.seed(seed)
-    sampled_paths = random.sample(all_paths, min(num_images, len(all_paths)))
+    dataset = dataset.map(lambda p: parse_image(p, img_size), num_parallel_calls=tf.data.AUTOTUNE)
     
-    # create dataset from paths
-    dataset = tf.data.Dataset.from_tensor_slices(sampled_paths)
+    if fixed_seed is not None:
+        np.random.seed(fixed_seed)
+        dataset = dataset.map(lambda img: add_masks(img, img_size, mask_type), num_parallel_calls=1)
+        dataset = dataset.cache()
+    else:
+        dataset = dataset.map(lambda img: add_masks(img, img_size, mask_type), num_parallel_calls=tf.data.AUTOTUNE)
     
-    # parse each image
-    def parse_image(path):
-        image = tf.io.read_file(path)
-        image = tf.image.decode_jpeg(image, channels=3)
-        image = tf.image.resize(image, [img_size, img_size])
-        image = tf.cast(image, tf.float32)
-        image = (image / 127.5) - 1.0
-        return image
+    if shuffle:
+        dataset = dataset.shuffle(buffer_size=1000, seed=fixed_seed or 42)
     
-    dataset = dataset.map(parse_image, num_parallel_calls=tf.data.AUTOTUNE)
-    dataset = dataset.map(lambda img : add_masks(img, img_size, mask_type), num_parallel_calls=tf.data.AUTOTUNE)
-    
-    # shuffle, batch, prefetch
-    dataset = dataset.shuffle(buffer_size=1000, seed=seed)
     dataset = dataset.batch(batch_size)
     dataset = dataset.prefetch(tf.data.AUTOTUNE)
     
