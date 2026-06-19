6781497f002fbc0714db4f2c4f51cc39ed60c9cc
RudraKsh0091 <rudraa.ln00@gmail.com>
2026-06-19 22:32:03 +0530

    Test Code

--- src/config.py
+++ src/config.py
@@ -1,7 +1,11 @@
 IMAGE_DIR = "/kaggle/input/datasets/awsaf49/coco-2017-dataset/coco2017/train2017"
 IMG_SIZE = 256
 BATCH_SIZE = 16
-EPOCHS = 30
-NUM_TRAIN = 36000
-NUM_VAL = 4000
+# EPOCHS = 30
+# NUM_TRAIN = 36000
+# NUM_VAL = 4000
 LEARNING_RATE = 2e-4
+
+NUM_TRAIN = 200
+NUM_VAL = 50
+EPOCHS = 2
